fee58380cafd3563be6661cbc719533165b4ff4c
B2gussain <bittuggussain@gmail.com>
2024-09-06 19:32:05 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  port:import.meta.PORT||process.env.PORT||5173,
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to make it accessible externally
+    port: process.env.PORT || 5173,  // Use Render's provided PORT or fallback to 5173 for local dev
+  },
   plugins: [react()],
 })
